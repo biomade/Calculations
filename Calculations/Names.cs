e94cc1bf0a3bf25268d293c63a2434ad70fda2c9
laurie kern <laurie.kern.ctr@us.af.mil>
2020-09-03 10:00:32 -0500

    assert string comparision

---
+++ Calculations/Names.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculations
+{
+    public class Names
+    {
+        public string MakeFullName(string first, string last)
+        {
+            return $"{first} {last}";
+        }
+
+        public string NickName { get; set; }
+    }
+}
---
+++ XUnitCalculations.Test/NamesTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitCalculations.Test
+{
+   public class NamesTests
+    {
+        [Fact]
+        public void MakeFullNameTest()
+        {
+            var name = new Calculations.Names();
+            var result = name.MakeFullName("Laurie", "Kern");
+            //Assert.Equal("Laurie Kern", result);
+
+            ////to ignore case
+            //Assert.Equal("Laurie Kern", result,ignoreCase:true);
+
+            ////for partial
+            //Assert.Contains("urie", result);
+
+            ////starts with
+            //Assert.StartsWith("Laur", result, StringComparison.InvariantCultureIgnoreCase);
+
+            ////can also use regular expressions
+            Assert.Matches("[A-Z]{1}[a-z]+[\\s][A-Z]{1}[a-z]+", result);
+        }
+
+        [Fact]
+        public void NickName_MustBeNull()
+        {
+            var name = new Calculations.Names();
+            Assert.Null(name.NickName);
+        }
+
+        [Fact]
+        public void NickName_MustBeNotNull()
+        {
+            var name = new Calculations.Names();
+            name.NickName = "laur";
+            Assert.NotNull(name.NickName);
+        }
+
+        [Fact]
+        public void MakeFullName_AlwaysReturnValue()
+        {
+            var name = new Calculations.Names();
+            var result = name.MakeFullName("Laurie", "Kern");
+            Assert.NotNull(result);
+            Assert.False(string.IsNullOrEmpty(result)); //so it can't be empty
+        }
+    }
+}

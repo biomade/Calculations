1d0e2023273c8a42cea056ee3db5cc66cd3e541b
laurie kern <laurie.kern.ctr@us.af.mil>
2020-09-03 09:44:16 -0500

    asserts numbers

--- XUnitCalculations.Test/CalculatorTests.cs
+++ XUnitCalculations.Test/CalculatorTests.cs
@@ -1,4 +1,7 @@
+﻿using Calculations;
 using System;
+using System.Collections.Generic;
+using System.Text;
 using Xunit;
 
 namespace XUnitCalculations.Test
@@ -6,9 +9,23 @@ namespace XUnitCalculations.Test
     public class CalculatorTests
     {
         [Fact]
-        public void TestAdd()
+        public void Add_GivenTwoIntValues_ReturnsIntSum()
         {
-            Assert.True(true);
+            var calc = new Calculator();
+            var result = calc.Add(1, 2);
+            Assert.Equal(3, result);
+        }
+
+        [Fact]
+        public void Add_GivenTwoDblValues_ReturnsDblSum()
+        {
+            var calc = new Calculator();
+            var result = calc.AddDouble(1.2, 2.3);
+            Assert.Equal(3.5, result);
+            
+            ////with precision
+            //result = calc.AddDouble(1.23, 2.34);
+            //Assert.Equal(3.57, result, 1); //rounds up
         }
     }
 }
---
+++ XUnitCalculations.Test/CalculatorTests1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XUnitCalculations.Test
+{
+    public class CalculatorTests1
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
